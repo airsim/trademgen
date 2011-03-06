612fb9478d2ab43f4de35d1d9933d06ed01d9da2
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-06 15:23:17 +0100

    [Dev] Suppressed the factory framework, as that of StdAir is now used.

--- trademgen/factory/FacTRADEMGENServiceContext.cpp
+++ trademgen/factory/FacTRADEMGENServiceContext.cpp
@@ -3,8 +3,9 @@
 // //////////////////////////////////////////////////////////////////////
 // STL
 #include <cassert>
-// TRADEMGEN Common
-#include <trademgen/factory/FacSupervisor.hpp>
+// StdAir
+#include <stdair/service/FacSupervisor.hpp>
+// TraDemGen
 #include <trademgen/factory/FacTRADEMGENServiceContext.hpp>
 #include <trademgen/service/TRADEMGEN_ServiceContext.hpp>
 
@@ -24,7 +25,7 @@ namespace TRADEMGEN {
       _instance = new FacTRADEMGENServiceContext();
       assert (_instance != NULL);
       
-      FacSupervisor::instance().registerServiceFactory (_instance);
+      stdair::FacSupervisor::instance().registerServiceFactory (_instance);
     }
     return *_instance;
   }
