968af92bf81a530c63a2e3a31e85eafa4bc7279b
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2011-03-06 16:22:23 +0100

    [Dev] Added copy constructors for CategoricalAttributeLite and ContinuousAttributeLite.

--- trademgen/basic/CategoricalAttributeLite.hpp
+++ trademgen/basic/CategoricalAttributeLite.hpp
@@ -5,46 +5,58 @@
 // Import section
 // //////////////////////////////////////////////////////////////////////
 // STL
-#include <map>
+#include <cassert>
 #include <iosfwd>
+#include <string>
+#include <map>
 // StdAir
 #include <stdair/stdair_basic_types.hpp>
 #include <stdair/service/Logger.hpp>
-// TRADEMGEN
+// TraDemGen
+#include <trademgen/TRADEMGEN_Exceptions.hpp>
 #include <trademgen/basic/DictionaryManager.hpp>
 
 namespace TRADEMGEN {
 
-  /** Class modeling the distribution of values that can be taken by a
-      categorical attribute. */
-  template <class T>
+  /**
+   * @brief Class modeling the distribution of values that can be
+   * taken by a categorical attribute.
+   */
+  template <typename T>
   struct CategoricalAttributeLite {
     public:
-    /** Define the probability mass function type. */
+    // ///////////// Type definitions //////////////
+    /**
+     * Type for the probability mass function.
+     */
     typedef std::map<T, stdair::Probability_T> ProbabilityMassFunction_T;
     
+
   public:
     // /////////////// Business Methods //////////
-    /** Get value from inverse cumulative distribution. */
-    const T getValue (stdair::Probability_T iCumulativeProbability) const {
-      const DictionaryKey_T lKey =
+    /**
+     * Get value from inverse cumulative distribution.
+     */
+    const T& getValue (const stdair::Probability_T& iCumulativeProbability) const {
+      const DictionaryKey_T& lKey =
         DictionaryManager::valueToKey (iCumulativeProbability);
 
       for (unsigned int idx = 0; idx < _size; ++idx) {
         if (_cumulativeDistribution[idx] >= lKey) {
-          return _valueArray[idx];
+          const T& oValue = _valueArray[idx];
+          return oValue;
         }
       }
 
-      STDAIR_LOG_ERROR ("The following cumulative probability "
-                        << "is out of range: " << iCumulativeProbability);
-      STDAIR_LOG_ERROR (displayProbabilityMass ());
-      assert (false);
-      T t;
-      return t;
+      std::ostringstream oStr;
+      oStr << "The following cumulative probability is out of range: "
+           << iCumulativeProbability << displayProbabilityMass();
+      throw IndexOutOfRangeException (oStr.str());
     }
 
-    /** Check if a value belongs to the value list. */
+    /**
+     * Check if a value belongs to the value list.
+     */
     bool checkValue (const T& iValue) const {
       for (unsigned int idx = 0; idx < _size; ++idx) {
         if (_valueArray[idx] == iValue) {
@@ -54,45 +66,69 @@ namespace TRADEMGEN {
       return false;
     }
     
+
   public:
     // ////////////// Display Support Methods //////////
-    /** Display probability mass function. */
-    const std::string displayProbabilityMass () const {
+    /**
+     * Display probability mass function.
+     */
+    const std::string displayProbabilityMass() const {
       std::ostringstream oStr;
 
       for (unsigned int idx = 0; idx < _size; ++idx) {
         if (idx != 0) {
           oStr << ", ";
         }
-        oStr << _valueArray [idx] << ":"
+        oStr << _valueArray[idx] << ":"
              << DictionaryManager::keyToValue (_cumulativeDistribution[idx]);
       }
       return oStr.str();
     }
         
+
   public:
     // ////////// Constructors and destructors /////////
-    /** Constructor by default */
+    /**
+     * Main constructor.
+     */
     CategoricalAttributeLite (const ProbabilityMassFunction_T& iValueMap)
       : _size (iValueMap.size()) {
       _cumulativeDistribution = new DictionaryKey_T[_size];
       _valueArray = new T[_size];
       init (iValueMap);
     }
-    /** Destructor */
-    virtual ~CategoricalAttributeLite () {
+
+    /**
+     * Copy constructor.
+     */
+    CategoricalAttributeLite (const CategoricalAttributeLite& iCAL)
+      : _size (iCAL._size),
+        _cumulativeDistribution (iCAL._cumulativeDistribution),
+        _valueArray (iCAL._valueArray) {
+    }
+
+    /**
+     * Destructor.
+     */
+    virtual ~CategoricalAttributeLite() {
       // TODO: Verify that the arrays are correctly cleaned.
       delete[] _cumulativeDistribution; _cumulativeDistribution = NULL;
       delete[] _valueArray; _valueArray = NULL;
     }
 
+
   private:
-    /** Default constructors. */
-    CategoricalAttributeLite ();
-    CategoricalAttributeLite (const CategoricalAttributeLite&);
+    /**
+     * Default constructor.
+     */
+    CategoricalAttributeLite() : _size(1) {
+      assert (false);
+    }
 
 
-    /** Initialise the two arrays from the given map. */
+    /**
+     * Initialise the two arrays from the given map.
+     */
     void init (const ProbabilityMassFunction_T& iValueMap) {
       
       stdair::Probability_T cumulative_probability_so_far = 0.0;
@@ -100,16 +136,18 @@ namespace TRADEMGEN {
 
       // Browse the map to retrieve the values and to build the
       // cumulative probabilities.
-      for (typename ProbabilityMassFunction_T::const_iterator itProbabilityMassFunction = iValueMap.begin();
+      for (typename ProbabilityMassFunction_T::const_iterator
+             itProbabilityMassFunction = iValueMap.begin();
            itProbabilityMassFunction != iValueMap.end();
            ++itProbabilityMassFunction, ++idx) {
         
         stdair::Probability_T attribute_probability_mass =
           itProbabilityMassFunction->second;
+
         if (attribute_probability_mass > 0) {
-          T attribute_value = itProbabilityMassFunction->first;
+          const T& attribute_value = itProbabilityMassFunction->first;
           cumulative_probability_so_far += attribute_probability_mass;
-          DictionaryKey_T lKey =
+          const DictionaryKey_T& lKey =
             DictionaryManager::valueToKey (cumulative_probability_so_far);
 
           // Build the two arrays.
@@ -121,13 +159,19 @@ namespace TRADEMGEN {
   
   private:
     // ////////// Attributes //////////
-    /** Size of the two arrays. */
+    /**
+     * Size of the two arrays.
+     */
     const unsigned int _size;
     
-    /** Cumulative dictionary-coded distribution. */
+    /**
+     * Cumulative dictionary-coded distribution.
+     */
     DictionaryKey_T* _cumulativeDistribution;
 
-    /** The corresponding values. */
+    /**
+       The corresponding values.
+    */
     T* _valueArray;
   };
 }
--- trademgen/basic/ContinuousAttributeLite.hpp
+++ trademgen/basic/ContinuousAttributeLite.hpp
@@ -5,33 +5,42 @@
 // Import section
 // //////////////////////////////////////////////////////////////////////
 // STL
+#include <cassert>
 #include <iosfwd>
 #include <string>
 #include <map>
 // StdAir
 #include <stdair/stdair_basic_types.hpp>
-// TRADEMGEN
+// TraDemGen
+#include <trademgen/TRADEMGEN_Exceptions.hpp>
 #include <trademgen/basic/DictionaryManager.hpp>
 
 namespace TRADEMGEN {
 
-  /** Class modeling the distribution of values that can be taken by a
-      continuous attribute. */
-  template <class T>
+  /**
+   * @brief Class modeling the distribution of values that can be
+   * taken by a continuous attribute.
+   */
+  template <typename T>
   struct ContinuousAttributeLite {
   public:
-    // ///////////// Type definitions //////////////
+    // ///////////////////// Type definitions ///////////////////////
+    /**
+     * Type for the probability mass function.
+     */
     typedef std::multimap<T, stdair::Probability_T> ContinuousDistribution_T;
     
   public:
-    // /////////////// Business Methods //////////
-    /** Get value from inverse cumulative distribution. */
-    const T getValue(const stdair::Probability_T& iCumulativeProbability) const{
-      const DictionaryKey_T lKey =
+    // ////////////////////// Business Methods ////////////////////
+    /**
+     * Get value from inverse cumulative distribution.
+     */
+    const T getValue(const stdair::Probability_T& iCumulativeProbability) const {
+      const DictionaryKey_T& lKey =
         DictionaryManager::valueToKey (iCumulativeProbability);
-      unsigned int idx = 0;
 
       // Find the first cumulative probablity value greater or equal to lKey.
+      unsigned int idx = 0;
       for (; idx < _size; ++idx) {
         if (_cumulativeDistribution[idx] > lKey) {
           break;
@@ -45,25 +54,32 @@ namespace TRADEMGEN {
         return _valueArray[idx-1];
       }
 
-      stdair::Probability_T lCumulativeCurrentPoint =
+      //
+      const stdair::Probability_T& lCumulativeCurrentPoint =
         DictionaryManager::keyToValue (_cumulativeDistribution[idx]);
-      stdair::Probability_T lCumulativePreviousPoint =
+      const T& lValueCurrentPoint = _valueArray[idx];
+
+      //
+      const stdair::Probability_T& lCumulativePreviousPoint =
         DictionaryManager::keyToValue (_cumulativeDistribution[idx-1]);
-      T lValueCurrentPoint = _valueArray[idx];
-      T lValuePreviousPoint = _valueArray[idx-1];
+      const T& lValuePreviousPoint = _valueArray[idx-1];
 
       if (lCumulativePreviousPoint == lCumulativeCurrentPoint) {
         return lValuePreviousPoint;
       }
 
-      return lValuePreviousPoint + (lValueCurrentPoint - lValuePreviousPoint)
+      T oValue = lValuePreviousPoint + (lValueCurrentPoint - lValuePreviousPoint)
         * (iCumulativeProbability - lCumulativePreviousPoint)
         / (lCumulativeCurrentPoint - lCumulativePreviousPoint);
+
+      return oValue;
     }
     
   public:
-    // ////////////// Display Support Methods //////////
-    /** Display cumulative distribution */
+    // ////////////// Display Support Methods ////////////////
+    /**
+     * Display cumulative distribution.
+     */
     const std::string displayCumulativeDistribution() const {
       std::ostringstream oStr;
       
@@ -71,16 +87,21 @@ namespace TRADEMGEN {
         if (idx != 0) {
           oStr << ", ";
         }
-        oStr << _valueArray[idx] << ":"
-             << DictionaryManager::keyToValue (_cumulativeDistribution[idx]);
+
+        const stdair::Probability_T& lProbability =
+          DictionaryManager::keyToValue (_cumulativeDistribution[idx]);
+      
+        oStr << _valueArray[idx] << ":" << lProbability;
       }
       return oStr.str();
     }
 
 
   public:
-    // ////////// Constructors and destructors /////////
-    /** Constructor */
+    // ////////// Constructors and destructors //////////////
+    /**
+     * Constructor.
+     */
     ContinuousAttributeLite (const ContinuousDistribution_T& iValueMap)
       : _size (iValueMap.size()) {
       _cumulativeDistribution = new DictionaryKey_T[_size];
@@ -88,22 +109,36 @@ namespace TRADEMGEN {
       init (iValueMap);
     }
     
-    /** Destructor */
+    /**
+     * Copy constructor.
+     */
+    ContinuousAttributeLite (const ContinuousAttributeLite& iCAL)
+      : _size (iCAL._size),
+        _cumulativeDistribution (iCAL._cumulativeDistribution),
+        _valueArray (iCAL._valueArray) {
+    }
+
+    /**
+     * Destructor.
+     */
     virtual ~ContinuousAttributeLite() {
       // TODO: Verify that the arrays are correctly cleaned.
-      delete _cumulativeDistribution; _cumulativeDistribution = NULL;
-      delete _valueArray; _valueArray = NULL;
+      delete[] _cumulativeDistribution; _cumulativeDistribution = NULL;
+      delete[] _valueArray; _valueArray = NULL;
     }
 
   private:
-    /** Default constructor. */
+    /**
+     * Default constructor.
+     */
     ContinuousAttributeLite() : _size(1) {
       assert (false);
     }
-    ContinuousAttributeLite (const ContinuousAttributeLite&);
 
-    /** Determine inverse cumulative distribution from cumulative
-        distribution (initialisation). */
+    /**
+     * Determine inverse cumulative distribution from cumulative
+     * distribution (initialisation).
+     */
     void init (const ContinuousDistribution_T& iValueMap) {
       unsigned int idx = 0;
 
@@ -111,8 +146,8 @@ namespace TRADEMGEN {
       for (typename ContinuousDistribution_T::const_iterator it =
              iValueMap.begin(); it != iValueMap.end(); ++it, ++idx) {
         
-        T attributeValue = it->first;
-        DictionaryKey_T lKey = DictionaryManager::valueToKey (it->second);
+        const T& attributeValue = it->first;
+        const DictionaryKey_T& lKey = DictionaryManager::valueToKey (it->second);
         
         // Build the two arrays.
         _cumulativeDistribution[idx] = lKey;
@@ -123,13 +158,19 @@ namespace TRADEMGEN {
 
   private:
     // ////////// Attributes //////////
-    /** Size of the two arrays. */
+    /**
+     * Size of the two arrays.
+     */
     const unsigned int _size;
     
-    /** Cumulative dictionary-coded distribution. */
+    /**
+     * Cumulative dictionary-coded distribution.
+     */
     DictionaryKey_T* _cumulativeDistribution;
 
-    /** The corresponding values. */
+    /**
+     * The corresponding values.
+     */
     T* _valueArray;
   };
   
